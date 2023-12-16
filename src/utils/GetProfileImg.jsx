b66540558c36c616db4db868efa08cbfe455164a
Davidermellino <ermellinodavide@gmail.com>
2023-12-16 10:51:22 +0100

    Updates

--- src/utils/GetProfileImg.jsx
+++
@@ -1,6 +0,0 @@
-export default function getProfileImg(fileUrl) {
-  if (!fileUrl) {
-    return "https://djwpyiaajdldabatncon.supabase.co/storage/v1/object/public/avatars/default_picture.jpg";
-  }
-  return `https://djwpyiaajdldabatncon.supabase.co/storage/v1/object/public/avatars/${fileUrl}`;
-}
